edc98c4c7f01225e050ddbc2e3e57ce213198a8a
Joanh Brown <jswish02@gmail.com>
2019-09-08 04:15:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-
+['dog','cat','pig']
 end
 
 def using_sort(array)
-
+['1','2','3'].sort
 end
 
 def using_reverse(array)
-
+["1","2","3"].reverse
 end
 
 def using_first(array)
-
+['1','2','3'].first
 end
 
 def using_last(array)
-
+['1''2','3'].last
 end
 
 def using_size(array)
-
+['1''2''3'].size
 end
