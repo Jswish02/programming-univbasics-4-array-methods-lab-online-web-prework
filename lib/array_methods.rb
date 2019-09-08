d88033790abdd2d2a558f5e45bb80cc6d9ff1fd4
Joanh Brown <jswish02@gmail.com>
2019-09-08 05:04:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def using_include(array, element)
-["wow", "I", "am", "really", "learning", "arrays!"].include? ("I")
+["wow", "I", "am", "really", "learning", "arrays!"].include? (element)
 end
 
 def using_sort(array)
